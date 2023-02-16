0228d520f8041a5d068809783627b28939291120
Katsiaryna Dremliuh <nerona.91@gmail.com>
2023-02-16 03:55:38 +0300

    HW_02

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-//#region Task 1.
+//#region Lesson_01
+
+// Task 1.
 let city = "Minsk";
 let country = "Belarus";
 let population = 9255524;
@@ -7,26 +9,27 @@ let hasSoccerStadium = true;
 console.log(getMotherland(city, country, population, hasSoccerStadium));
 
 function getMotherland(city, country, population, hasSoccerStadium) {
-  return `Hello!\nI'am from ${city}, ${country}.\nThe population of ${country} is ${population} people.\nDo we have a soccer stadium in the city? Yes, that's ${hasSoccerStadium}.`;
+  return `Hello!\nI am from ${city}, ${country}.\nThe population of ${country} is ${population} people.\nDo we have a soccer stadium in the city? Yes, that's ${hasSoccerStadium}.`;
 }
-//#endregion
 
-//#region Task 2.
+//Task 2.
 let height = 40;
 let width = 70;
 let area = height * width;
 
 console.log(`The area of the rectangle: ${area} sq cm.`);
-//#endregion
 
-//#region Task 3.
+// Task 3.
 let speedOfFirst = 95;
 let speedOfSecond = 114;
 let time = 2;
-let closingSpeed = getClosingSpeed(speedOfFirst, speedOfSecond);
-let distance = getDistance(closingSpeed, time);
 
-console.log(`There is ${distance} km between cars.`);
+console.log(
+  `There is ${getDistance(
+    getClosingSpeed(speedOfFirst, speedOfSecond),
+    time
+  )} km between cars.`
+);
 
 function getClosingSpeed(speed1, speed2) {
   return speed1 + speed2;
@@ -35,9 +38,8 @@ function getClosingSpeed(speed1, speed2) {
 function getDistance(closingSpeed, time) {
   return closingSpeed * time;
 }
-//#endregion
 
-//#region Task 4.
+// Task 4.
 const randomNumber = Math.floor(Math.random() * 100);
 console.log(randomNumber);
 
@@ -48,9 +50,8 @@ if (randomNumber < 20) {
 } else {
   console.log("randomNumber больше 20, и меньше 50");
 }
-//#endregion
 
-//#region Task 4.
+// Task 5.
 let message;
 
 switch (true) {
@@ -66,4 +67,144 @@ switch (true) {
 }
 
 console.log(message);
+//#endregion
+
+//#region Lesson_02
+
+// Task 1.
+for (let i = 10; i <= 50; i++) {
+  if (isEven(i)) {
+    console.log(i);
+  }
+}
+
+function isEven(number) {
+  return number % 2 == 0;
+}
+
+// Task 2.
+const person = {
+  firstName: "Katya",
+  lastName: "Dremliuh",
+  age: 32,
+  hasPets: false,
+};
+
+for (var field in person) {
+  var value = person[field];
+  console.log(`${field} ${String.fromCodePoint(0x1f914)} ${value}`);
+}
+
+// Task 3.
+
+// using System.Text;
+
+// namespace ConsoleApp6
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.OutputEncoding = Encoding.UTF8;
+//             Console.InputEncoding = Encoding.Unicode;
+//             string text =
+//                 "В ту же ночь приехал я в Симбирск, где должен был пробыть сутки для закупки нужных вещей, что и было поручено Савельичу. Я остановился в трактире. Савельич с утра отправился по лавкам";
+
+//             string[] mixed =
+//             {
+//                 "я в Симбирск,",
+//                 "в трактире.",
+//                 "с утра",
+//                 "В ту же ночь",
+//                 "Я остановился",
+//                 "для закупки",
+//                 "что и было поручено Савельичу.",
+//                 "приехал",
+//                 "где должен был",
+//                 "нужных вещей",
+//                 "отправился по лавкам",
+//                 "пробыть сутки",
+//                 "Савельич",
+//             };
+
+//             var dictionary = PutPhrasesInOrder(mixed, text);
+
+//             StringBuilder sb = new StringBuilder();
+//             foreach (var word in dictionary)
+//             {
+//                 sb.Append($"{word.Value} ");
+//             }
+
+//             var result = sb.ToString().TrimEnd();
+
+//             Console.Write(result);
+//         }
+
+//         private static SortedDictionary<int, string> PutPhrasesInOrder(string[] hell, string validText)
+//         {
+//             SortedDictionary<int, string> dictionary = new();
+
+//             foreach (string word in hell)
+//             {
+//                 dictionary.Add(key: validText.IndexOf(word, StringComparison.Ordinal), value: word);
+//             }
+
+//             return dictionary;
+//         }
+//     }
+// }
+
+let text =
+  "В ту же ночь приехал я в Симбирск, где должен был пробыть сутки для закупки нужных вещей, что и было поручено Савельичу. Я остановился в трактире. Савельич с утра отправился по лавкам";
+
+const mixed = [
+  "я в Симбирск,",
+  "в трактире.",
+  "с утра",
+  "В ту же ночь",
+  "Я остановился",
+  "для закупки",
+  "что и было поручено Савельичу.",
+  "приехал",
+  "где должен был",
+  "нужных вещей,",
+  "отправился по лавкам",
+  "пробыть сутки",
+  "Савельич",
+];
+
+let dictionary = {};
+for (let i = 0; i < mixed.length; i++) {
+  let value = mixed[i];
+  let key = text.indexOf(mixed[i]);
+
+  dictionary[key] = value;
+}
+
+let result = "";
+for (var key in dictionary) {
+  console.log(key + " : " + dictionary[key]);
+  result += `${dictionary[key]} `;
+}
+
+result = result.trim();
+console.log(`Итог:\n${result}`);
+
+// Task 4.
+const getFullName = (firstName, lastName) => {
+  const fullName = `${firstName} ${lastName}`;
+  console.log(fullName);
+};
+
+getFullName("Katya", "Dremliuh");
+
+// Task 5.
+let start = 21;
+let end = 67;
+while (start <= end) {
+  if (!isEven(start)) {
+    console.log(start);
+  }
+  start++;
+}
 //#endregion
